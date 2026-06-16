20acee035967cfb2712844ff7336edc6981d0ad8
carolineboraczynskisilva-ship-it <caroline.boraczynski.silva@escola.pr.gov.br>
2026-06-16 07:54:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+// Seleciona todos os cards
+const cards = document.querySelectorAll(".card");
 
+// Adiciona evento de clique em cada card
+cards.forEach(card => {
+    card.addEventListener("click", () => {
+        card.classList.toggle("flip");
+    });
+});
+
+// Exemplo de mensagem no console
+console.log("JavaScript carregado com sucesso!");
